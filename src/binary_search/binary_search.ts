a87e4004b0220541696ed40fbea9509e6313906c
渡邉 竜太 <ryuta-watanabe@watanabenoMacBook-Pro.local>
2023-04-13 20:27:16 +0900

    二分探索問題クリア

--- src/binary_search/binary_search.ts
+++ src/binary_search/binary_search.ts
@@ -1,2 +1,17 @@
 export const binarySearch = (array: number[], target: number): number => {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (array[mid] === target) return mid;
+    if (array[mid] < target) {
+      left = mid + 1;
+    }
+    if (array[mid] > target) {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
 };
