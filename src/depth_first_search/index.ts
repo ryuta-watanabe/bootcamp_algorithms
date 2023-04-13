b406ca29c05511001d2ec8f2879bec0fdb842d71
渡邉 竜太 <ryuta-watanabe@watanabenoMacBook-Pro.local>
2023-04-14 08:48:32 +0900

    ファイルツリーの課題をクリア

--- src/depth_first_search/index.ts
+++ src/depth_first_search/index.ts
@@ -4,8 +4,23 @@ export interface FileSystemNode {
   children: FileSystemNode[];
 }
 
+// スタックを使う方法と再帰関数を使う方法がある
 export const depthFirstSearch = (
   root: FileSystemNode,
   target: string
 ): boolean => {
+  const stack = [root];
+
+  while (stack.length > 0) {
+    const node = stack.pop() as FileSystemNode;
+
+    if (node.name === target && !node.isDirectory) return true;
+    if (node.isDirectory) {
+      for (const child of node.children) {
+        stack.push(child);
+      }
+    }
+  }
+
+  return false;
 };
