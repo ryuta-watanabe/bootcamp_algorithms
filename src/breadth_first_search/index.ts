0181ffbaf841ea9759712090abc056de954ab0b2
渡邉 竜太 <ryuta-watanabe@watanabenoMacBook-Pro.local>
2023-04-14 09:18:02 +0900

    友達検索課題をクリア

--- src/breadth_first_search/index.ts
+++ src/breadth_first_search/index.ts
@@ -3,4 +3,26 @@ export const breadthFirstSearch = (
   startUser: number,
   targetUser: number
 ): number => {
+  const queue = [startUser];
+  const checked = new Set<number>();
+  let distance = 0;
+
+  while (queue.length > 0) {
+    const queueLength = queue.length;
+
+    for (let i = 0; i < queueLength; i++) {
+      const user = queue.shift() as number;
+
+      if (user === targetUser) return distance;
+      checked.add(user);
+
+      for (const friend of relationships[user]) {
+        if (!checked.has(friend)) queue.push(friend);
+      }
+    }
+
+    distance++;
+  }
+
+  return -1;
 };
